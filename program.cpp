85b866879004e28a79d65f93786889eac078ac28
Shahid Hasan <xiahid@yahoo.com>
2019-07-09 20:44:05 +0600

    add a c++ code

---
+++ program.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+
+#define loop(n) for(int i=0; i<n; i++)
+
+typedef long long ll;
+
+using namespace std;
+
+int main()
+{
+    cout<< "Hello Git" << endl;
+
+    return 0;
+}
